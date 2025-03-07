1b373b0eda505d323c73e848391c76731beff6c8
Lucas Duarte <lucs.rkt@gmail.com>
2025-03-07 16:28:58 -0300

    feat: exercise 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_07_183416) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_07_190925) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
     t.decimal "total_gross"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.datetime "released_on"
   end
 end
