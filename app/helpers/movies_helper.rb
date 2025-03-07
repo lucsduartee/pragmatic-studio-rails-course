7902df22e423bc53c6d2494fb0728b565e47b6af
Lucas Duarte <lucs.rkt@gmail.com>
2025-03-07 16:45:55 -0300

    feat: view helper exercise

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+  def day_and_time(movie)
+    movie.released_on.strftime("%Y")
+  end
+
+  def truncate_title(movie)
+    truncate(movie.description, length: 40, separator: " ")
+  end
 end
