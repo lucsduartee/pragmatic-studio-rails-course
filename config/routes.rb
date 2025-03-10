4896e96e801dcdc9c8c6f06ac85db1f4d636b4f3
Lucas Duarte <lucs.rkt@gmail.com>
2025-03-10 18:52:41 -0300

    feat: from exercise 25 to 30

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,9 @@ Rails.application.routes.draw do
   resources :movies do
     resources :reviews
   end
+
+  resource :session, only: [ :new, :create, :destroy ]
+
+  resources :users
+  get "signup" => "users#new"
 end
